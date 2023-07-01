19840ae8785abd2c2f8119cdfedefaeccbc4f1d7
Simon Njuguna Wanjira <snwnjuguna97@gmail.com>
2023-07-01 11:04:57 +0300

    My Commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(fun = 'roller-skate') {
+    return `This Saturday, I want to ${fun}!`;
+}
+saturdayFun();
+
+function mondayWork(work = 'go to the office') {
+    return `This Monday, I will ${work}.`;
+}
+mondayWork();
+
+function wrapAdjective(symb) {
+    return function (msg) {
+        return `You are ${symb}${msg}${symb}!`;
+    }    
+}
+wrapAdjective('*')("a hard worker");
+
